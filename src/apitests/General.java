1a9de6b874daf0627dede63def9c53d83a45f2fb
Lukáš Moláček <lukas@molacek.net>
2018-11-04 13:14:56 +0100

    Basic JSON parsing

--- src/apitests/General.java
+++ src/apitests/General.java
@@ -3,12 +3,25 @@ package apitests;
 import org.testng.annotations.Test;
 
 import httpclient.HttpClient;
+import org.json.JSONObject;
+import java.util.Iterator;
 
 public class General {
 
 	@Test
 	public void Test_A() {
 		HttpClient http_client = new HttpClient();
-		System.out.println(http_client);
+		try {
+			http_client.get("https://images-api.nasa.gov/search?q=center");
+			JSONObject responseJson = new JSONObject(http_client.responseText());
+			JSONObject responseCollection = (JSONObject)responseJson.get("collection");
+		    Iterator<String> it = responseCollection.keys();
+		    while(it.hasNext()) {
+		    	System.out.println(it.next());
+		    }
+			
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
 	}
 }
